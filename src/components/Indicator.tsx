911cf04647e05118e8c72f969f37961fc94aa6c1
AsovaS-pro <44199920+AsovaS-pro@users.noreply.github.com>
2019-08-06 17:15:29 +0300

    Update Indicator.tsx

--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -1,27 +1,31 @@
+// @flow
+
 import React from 'react';
 import { Animated } from 'react-native';
 import styled from 'styled-components/native';
 
-import constants from '../lib/constants';
+import values from '../lib/values';
 
-interface BarProps {
-  tabWidth: number;
-  color: string;
-}
+type BarProps = {
+  tabWidth: number,
+  color: string,
+};
 
 const Bar = styled(Animated.View)`
-  height: ${constants.indicatorHeight};
+  height: ${values.indicatorHeight};
   width: ${(props: BarProps) => props.tabWidth};
+  borderTopLeftRadius: ${values.indicatorHeight};
+  borderTopRightRadius: ${values.indicatorHeight};
   position: absolute;
   bottom: 0;
   background-color: ${(props: BarProps) => props.color};
 `;
 
-interface IndicatorProps {
-  color: string;
-  tabWidth: number;
-  value: Animated.Value;
-}
+type IndicatorProps = {
+  color: string,
+  tabWidth: number,
+  value: Animated.Value,
+};
 
 const Indicator = (props: IndicatorProps) => (
   <Bar
